7e4b92573b63a230b7d7db2c19a13e0fdbe1e33f
NavindaFernando <m.navindafernando@gmail.com>
2022-05-08 01:22:31 -0700

    Created response AppWideExceptionHandler class

---
+++ src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.advisor;
+
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e) {
+        return new ResponseUtil(500, e.getMessage(), null);
+    }
+}
