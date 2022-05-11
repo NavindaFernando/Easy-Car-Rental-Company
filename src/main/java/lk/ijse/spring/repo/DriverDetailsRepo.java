f6b231a612ad0d56b3e748f4076e98903271f761
NavindaFernando <m.navindafernando@gmail.com>
2022-05-11 08:59:31 -0700

    Created all repo interfaces

---
+++ src/main/java/lk/ijse/spring/repo/DriverDetailsRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.DriverDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverDetailsRepo extends JpaRepository<DriverDetails,String> {
+
+}
